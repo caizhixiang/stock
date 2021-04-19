92f2c8b00d5ec22806d5407f774f5014d1b67310
caizhixiang <caizhixiang1989@163.com>
2021-04-20 00:32:00 +0800

    初始化

---
+++ config_default.py
@@ -0,0 +1,16 @@
+# config_default.py
+'''
+Default configurations.
+'''
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ orm/models.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'caizhixiang'
+
+import time, uuid
+
+from orm.orm import Model,IntegerField, StringField, BooleanField, FloatField, TextField
+
+
+class User(Model):
+    __table__ = 'users'
+
+    id = IntegerField(primary_key=True)
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
