728524338d3e4be660fd6abc6865efe92d95f037
caizhixiang <caizhixiang1989@163.com>
2021-05-25 01:38:07 +0800

    动态分页

--- industry/industry/orm/test.py
+++ industry/industry/orm/test.py
@@ -1,10 +1,31 @@
-from industry.orm.models import IndustryInfo
-from industry.orm import  orm
-import asyncio
+from twisted.enterprise import adbapi
+from twisted.internet import reactor
+
+from industry.settings import MY_MYSQL_SETTINGS
+from pymysql import cursors
 
 
-orm.create_pool()
+class orm(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+        self.dicc = []
 
-u = IndustryInfo()
-find = u.find(IndustryInfo, 1)
-print(find)
+    def getAge(self, id):
+        query = self.dbpool.runQuery("SELECT * FROM industry_info WHERE id = %s", id)
+        return query.addCallbacks(self.printResult)
+
+    def printResult(self, l):
+        for item in l:
+            print(item)
+            self.dicc.append(item['id'])
+
+
+asyn_mysql_settings = MY_MYSQL_SETTINGS
+asyn_mysql_settings['cursorclass'] = cursors.DictCursor
+dbpool = adbapi.ConnectionPool("pymysql", **asyn_mysql_settings)
+o = orm(dbpool)
+o.getAge(1)
+reactor.callLater(4, reactor.stop)
+reactor.run()
+print('-------------')
+print(o.dicc)
